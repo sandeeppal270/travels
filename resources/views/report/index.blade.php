07dce7fbf5ac70d4881c7d00b43852724340f171
sandeeppal270 <64672009+sandeeppal270@users.noreply.github.com>
2022-10-18 11:52:26 +0530

    first commit

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -10,9 +10,9 @@
 
             <!-- /menu footer buttons -->
             <div class="sidebar-footer hidden-small">
-              <a data-toggle="tooltip" data-placement="top" title="Settings">
+              {{-- <a data-toggle="tooltip" data-placement="top" title="Settings">
                 <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
-              </a>
+              </a> --}}
               <a onclick="closeFullscreen();" title="Close FullScreen" data-toggle="tooltip" data-placement="top"><span aria-hidden="true"><b>X</b></span></a>
               <a onclick="openFullscreen();" data-toggle="tooltip" data-placement="top" title="FullScreen">
                 <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
